2ebb7ff6576bede0d8e5078604dd035d937afdf6
DanielRuch <44618830+DanielRuch@users.noreply.github.com>
2020-02-15 12:45:40 -0600

    Updates

---
+++ precache-manifest.2aa43e88c30cc7ecb450ad542e3c805a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c75320f8f494d4c11cef8ad681fa22a",
+    "url": "/Portfolio-App/index.html"
+  },
+  {
+    "revision": "1d5d6f56462ac6b58433",
+    "url": "/Portfolio-App/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5afd0cea751477099bad",
+    "url": "/Portfolio-App/static/css/main.e57d712f.chunk.css"
+  },
+  {
+    "revision": "1d5d6f56462ac6b58433",
+    "url": "/Portfolio-App/static/js/2.b9401113.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/Portfolio-App/static/js/2.b9401113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afd0cea751477099bad",
+    "url": "/Portfolio-App/static/js/main.fce82770.chunk.js"
+  },
+  {
+    "revision": "b7b7ce2a82d23558c58d",
+    "url": "/Portfolio-App/static/js/runtime-main.99215042.js"
+  },
+  {
+    "revision": "bb6f60d3e376d61121600437cdf23d41",
+    "url": "/Portfolio-App/static/media/pandas-project-fantasy.bb6f60d3.png"
+  }
+]);
